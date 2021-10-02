cd9fe118f356eaf443eaa946373eb31e8b098755
Md Jubayer <jubayer.sust.23@gmail.com>
2021-10-02 11:02:31 +0100

    Initial commit

---
+++ app/src/main/java/com/sprytech/birthdaychallenge/BirthdayChallengeApplication.kt
@@ -0,0 +1,9 @@
+package com.sprytech.birthdaychallenge
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+open class BirthdayChallengeApplication : Application() {
+
+}
